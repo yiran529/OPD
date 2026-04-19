cf00edb06be36e38dd2f3d5529dc9deeda1aefff
yiran529 <yiran0834@gmail.com>
2026-04-19 22:43:48 +0800

    update gsm8k evaluation script

--- LinearOPSD/eval/run_eval_gsm8k.sh
+++ LinearOPSD/eval/run_eval_gsm8k.sh
@@ -9,15 +9,41 @@ NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 "$PYTHON_BIN" evaluate_gsm8k.py 
     --tensor_parallel_size 4 \
     --prompt_style chat \
     --enable_thinking \
-    --temperature 0.6 \
+    --temperature 1.0 \
     --top_p 0.95 \
     --max_new_tokens 2048 \
-    --val_n 1
+    --val_n 12
 wait
 
 # After training, uncomment and set CHECKPOINT_DIR to evaluate a LoRA checkpoint.
-# CHECKPOINT_DIR="/path/to/checkpoint"
-# NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 "$PYTHON_BIN" evaluate_gsm8k.py \
+CHECKPOINT_DIR="/data/wyr/LinearOPSD/outputs/linear_opsd/qwen35_0p8b_linear_opsd/checkpoint-200/adapter_model.safetensors"
+NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=4,5,6,7 "$PYTHON_BIN" evaluate_gsm8k.py \
+    --base_model "$BASE_MODEL" \
+    --checkpoint_dir "$CHECKPOINT_DIR" \
+    --tensor_parallel_size 4 \
+    --prompt_style chat \
+    --enable_thinking \
+    --temperature 1.0 \
+    --top_p 0.95 \
+    --max_new_tokens 2048 \
+    --val_n 12
+wait
+
+CHECKPOINT_DIR="/data/wyr/LinearOPSD/outputs/linear_opsd/qwen35_0p8b_linear_opsd/checkpoint-100/adapter_model.safetensors"
+NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=4,5,6,7 "$PYTHON_BIN" evaluate_gsm8k.py \
+    --base_model "$BASE_MODEL" \
+    --checkpoint_dir "$CHECKPOINT_DIR" \
+    --tensor_parallel_size 4 \
+    --prompt_style chat \
+    --enable_thinking \
+    --temperature 1.0\
+    --top_p 0.95 \
+    --max_new_tokens 2048 \
+    --val_n 12
+wait
+
+# CHECKPOINT_DIR="/data/wyr/LinearOPSD/outputs/linear_opsd/qwen35_0p8b_linear_opsd/checkpoint-150/adapter_model.safetensors"
+# NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES=4,5,6,7 "$PYTHON_BIN" evaluate_gsm8k.py \
 #     --base_model "$BASE_MODEL" \
 #     --checkpoint_dir "$CHECKPOINT_DIR" \
 #     --tensor_parallel_size 4 \
